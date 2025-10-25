e737356e01a0f5ef79a466b6aa701240d08044db
Song Khoi <songkhoi123@gmail.com>
2025-10-25 15:22:40 +0700

    fix

--- src/tasks/worker.py
+++ src/tasks/worker.py
@@ -45,7 +45,7 @@ celery_app.conf.update(
         },
         "crm-data-sync": {
             "task": "src.crm.tasks.sync_crm_data",
-            "schedule": crontab(hour=11, minute=55),  # Run at 8:30 AM daily
+            "schedule": crontab(hour=15, minute=40),  # Run at 8:30 AM daily
             "args": (),
         },
     },
