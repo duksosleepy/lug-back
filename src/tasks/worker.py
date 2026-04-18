3ff12d6320b5e9429f567dd2e2c54fcb281e5c0f
Song Khoi <songkhoi123@gmail.com>
2026-04-18 09:27:04 +0700

    fix retry time

--- src/tasks/worker.py
+++ src/tasks/worker.py
@@ -46,7 +46,7 @@ celery_app.conf.update(
         },
         "crm-data-sync": {
             "task": "src.crm.tasks.sync_crm_data",
-            "schedule": crontab(hour=8, minute=30),
+            "schedule": crontab(hour=9, minute=35),
             "args": (),
         },
     },
