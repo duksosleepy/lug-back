07a910ef3bb586716484589998c23d2b1b83a596
Song Khoi <songkhoi123@gmail.com>
2026-04-26 22:54:14 +0700

    fix journal retention time

--- src/tasks/worker.py
+++ src/tasks/worker.py
@@ -46,7 +46,7 @@ celery_app.conf.update(
         },
         "crm-data-sync": {
             "task": "src.crm.tasks.sync_crm_data",
-            "schedule": crontab(hour=9, minute=40),
+            "schedule": crontab(hour=23, minute=5),
             "args": (),
         },
     },
