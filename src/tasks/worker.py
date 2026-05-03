a2d6515e8123fa928af9bdad9a26ed2f0269640f
Song Khoi <songkhoi123@gmail.com>
2026-05-03 23:25:09 +0700

    recovery time

--- src/tasks/worker.py
+++ src/tasks/worker.py
@@ -46,7 +46,7 @@ celery_app.conf.update(
         },
         "crm-data-sync": {
             "task": "src.crm.tasks.sync_crm_data",
-            "schedule": crontab(hour=11, minute=20),
+            "schedule": crontab(hour=7, minute=0),
             "args": (),
         },
     },
